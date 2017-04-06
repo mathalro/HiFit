56732d3c93ed6134d1872307f2bdd82001084a0d
Aluno <aluno@decom-OptiPlex-780>
2017-04-06 13:12:31 -0300

    Alteracoes finais no banco, revisao do dia 05-04

--- HiFit/allmodels.py
+++ HiFit/allmodels.py
@@ -21,61 +21,52 @@ class Atividade(models.Model):
 class Caracteristica(models.Model):
     descricao = models.CharField(max_length=90)
     valor = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    tipo = models.IntegerField()
 
     def __unicode__(self):
         return self.descricao
     
 
-# class Caracteristicaaluno(models.Model):
-#     caracteristica_idcaracteristica = models.ForeignKey(Caracteristica, models.DO_NOTHING, db_column='Caracteristica_idCaracteristica')  # Field name made lowercase.
-#     usuario_login = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='Usuario_login')  # Field name made lowercase.
-
-#     class Meta:
-#         managed = False
-#         db_table = 'CaracteristicaAluno'
-#         unique_together = (('caracteristica_idcaracteristica', 'usuario_login'),)
-
-
 class Classificacao(models.Model):
-    somanota = models.IntegerField(db_column='somaNota')  # Field name made lowercase.
-    somapessoas = models.IntegerField(db_column='somaPessoas')  # Field name made lowercase.
+    somanota = models.IntegerField()  # Field name made lowercase.
+    somapessoas = models.IntegerField()  # Field name made lowercase.
 
 
 class Comentario(models.Model):
-    conteudo = models.TextField()
+    conteudo = models.TextField(max_length=140)
     data = models.DateField(auto_now=False, auto_now_add=True)
-    post_idpost = models.ForeignKey('Post', models.DO_NOTHING, db_column='Post_idPost')  # Field name made lowercase.
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # Field name made lowercase.
 
     def __unicode__(self):
-        return self.conteudo
+        return self.data + ': ' +self.conteudo
 
 
 class Denuncia(models.Model):
     titulo = models.CharField(max_length=50)
-    conteudo = models.TextField()
+    conteudo = models.TextField(max_length=500)
     data = models.DateField(auto_now_add=True)
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE, related_name='denuncias')  # Field name made lowercase.
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='denuncias')  # Field name made lowercase.
 
     def __unicode__(self):
         return self.titulo
 
 
 class Mensagem(models.Model):
-    conteudo = models.TextField()
+    conteudo = models.TextField(max_length=140)
     data = models.DateField(auto_now_add=True)
-    remetente = models.ForeignKey('Usuario', related_name='mensagens')  # Field name made lowercase.
-    destinatario = models.ForeignKey('Usuario', related_name='mensagens')  # Field name made lowercase.    
+    remetente = models.ForeignKey(Usuario, related_name='mensagens')  # Field name made lowercase.
+    destinatario = models.ForeignKey(Usuario, related_name='mensagens')  # Field name made lowercase.    
 
 
 class Post(models.Model):
-    conteudo = models.TextField()
+    conteudo = models.TextField(max_length=500)
     data = models.DateField(auto_now_add=True)
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE, related_name='posts')  # Field name made lowercase.
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='posts')  # Field name made lowercase.
     classificacao = models.ForeignKey(Classificacao, on_delete=models.CASCADE)  # Field name made lowercase.
 
-
+    
 class Profissao(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=40)
 
     __unicode__(self):
         return nome
@@ -84,10 +75,11 @@ class Profissao(models.Model):
 class Recomendacao(models.Model):
     data = models.DateField(auto_now_add=True)  # Field name made lowercase.
     classificacao = models.ForeignKey(Classificacao, on_delete=models.CASCADE)  # Field name made lowercase.
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE, related_name='recomendacoes')  # Field name made lowercase.
-
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='recomendacoes')  # Field name made lowercase.
+    regras = models.ManyToManyField(Regra, on_delete=models.SET_NULL, 'recomendacoes')
 
 class Regra(models.Model):
+    dono = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     pontuacao = models.IntegerField()
     restricao = models.ForeignKey(Caracteristica, on_delete=models.CASCADE)
     beneficios = models.ForeignKey(Caracteristica, on_delete=models.CASCADE)
@@ -95,23 +87,19 @@ class Regra(models.Model):
     datacriacao = models.DateField(auto_now_add=True)  # Field name made lowercase.
     atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE)  # Field name made lowercase.
 
-class Sugestao(models.Model):
-    conteudo = models.TextField()
-    data = models.DateField(auto_now_add)
 
-    class Meta:
-        managed = False
-        db_table = 'Sugestao'
+class Sugestao(models.Model):
+    conteudo = models.TextField(max_length=500)
+    data = models.DateField(auto_now_add=True)
 
 
 class Usuario(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    login = models.CharField(max_length=45)
     tipo_usuario = models.IntegerField()  # Field name made lowercase.
-    email = models.EmailField(max_length=100)
     datanascimento = models.DateField()  # Field name made lowercase.
     telefone = models.CharField(max_length=20)
     classificacao = models.ForeignKey(Classificacao, on_delete=models.CASCADE)  # Field name made lowercase.
     cpf = models.CharField(max_length=20)
     identificacao = models.CharField(max_length=45)
-    profissao = models.ForeignKey(Profissao, on_delete=models.CASCADE)  # Field name made lowercase.
+    profissao = models.ForeignKey(Profissao, on_delete=models.CASCADE, related_name='usuarios')  # Field name made lowercase.
+    caracteristicas = models.ManyToManyField(Caracteristica, on_delete=models.SET_NULL, null=True,blank=True)
