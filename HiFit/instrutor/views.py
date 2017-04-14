5f3be8b4ee836922a1772c07cb579ea2836a5d0f
Vinicius Magalhaes <viniciusvmda@gmail.com>
2017-04-14 12:07:59 +0000

    Modelado o Cadastro da tela de Regras

--- HiFit/instrutor/views.py
+++ HiFit/instrutor/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from usuario.models import Atividade
+from aluno.models import Caracteristica
 
 # Create your views here.
+# Tela de Regras
+def regras(request):
+    atividades = Atividade.objects.all()
+    restricoes = Caracteristica.objects.filter()
+    return render(request,'regras.html', {"atividades": atividades})
