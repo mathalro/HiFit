228abba0aee8bfa69ee4dd7ca448a19deb2a0dbc
Pehesi97 <pedro.henrique.silva.1997@outlook.com.br>
2017-04-15 18:39:52 -0300

    Consertando campo de formulário

--- HiFit/usuario/forms.py
+++ HiFit/usuario/forms.py
@@ -8,4 +8,4 @@ TIPO_CHOICES = (
 class FaleConoscoForm(forms.Form):
     tipo = forms.ChoiceField(required=True, choices = TIPO_CHOICES)
     assunto = forms.CharField(required=True, max_length=100)
-    conteudo = forms.CharField(required=True, max_length=500, widget=forms.Textarea, name="Conteúdo")    
+    conteudo = forms.CharField(required=True, max_length=500, widget=forms.Textarea)    
