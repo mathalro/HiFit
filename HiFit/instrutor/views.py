0e9e5e7ccd805bf3b269ff710ac8f82521cc8c6a
Higor Coimbra <higor1308@gmail.com>
2017-04-24 09:18:19 -0300

    Incluindo cabecalho do instrutor na tela de cadastro

--- HiFit/instrutor/views.py
+++ HiFit/instrutor/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from .forms import *
 from utils.tipos import TIPO
 
 # Create your views here.
+@login_required
 def cadastroInstrutor(request):
 	if request.method == 'POST':
 		cadastroDadosTecnicos = FormularioDadosTecnicos(request.POST)
