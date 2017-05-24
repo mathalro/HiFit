6b6aa3f2611b270bab7275d0fdcf022981e74111
Higor Coimbra <higor1308@gmail.com>
2017-05-24 09:53:17 -0300

    corrigindo erros na tela de relatorios

--- HiFit/usuario/migrations/0009_post_privacidade.py
+++ HiFit/usuario/migrations/0009_post_privacidade.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('usuario', '0008_usuario_tipo_identificacao'),
+        ('usuario', '0009_auto_20170521_1454'),
     ]
 
     operations = [
